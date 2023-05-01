992055a69e756c92c3186ee7462e674c6333ad10
SoDebug <30922923+SoDebug@users.noreply.github.com>
2023-05-01 22:25:05 +0800

    setup.sh: Adapt to SOVIET-ANDROID kernel compilation command

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -7,15 +7,15 @@ echo "[+] GKI_ROOT: $GKI_ROOT"
 
 if test -d "$GKI_ROOT/common/drivers"; then
      DRIVER_DIR="$GKI_ROOT/common/drivers"
-elif test -d "$GKI_ROOT/drivers"; then
-     DRIVER_DIR="$GKI_ROOT/drivers"
+elif test -d "$GKI_ROOT/drivers/staging/"; then
+     DRIVER_DIR="$GKI_ROOT/drivers/staging"
 else
-     echo '[ERROR] "drivers/" directory is not found.'
+     echo '[ERROR] "drivers/staging" directory is not found.'
      echo '[+] You should modify this scrpit by yourself.'
      exit 127
 fi
 
-test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/SoDebug/KernelSU
+test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/SoDebug/KernelSU -b SOVIET-ANDROID
 cd "$GKI_ROOT/KernelSU"
 git stash
 if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
@@ -32,7 +32,7 @@ cd "$GKI_ROOT"
 echo "[+] GKI_ROOT: $GKI_ROOT"
 echo "[+] Copy kernel su driver to $DRIVER_DIR"
 
-test -e "$DRIVER_DIR/kernelsu" || ln -sf "$GKI_ROOT/KernelSU/kernel" "$DRIVER_DIR/kernelsu"
+test -e "$DRIVER_DIR/kernelsu" || ln -sf "$GKI_ROOT/KernelSU" "$DRIVER_DIR/kernelsu"
 
 echo '[+] Add kernel su driver to Makefile'
 
